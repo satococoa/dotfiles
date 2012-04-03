4ec2e4e1b91a43778853f63b7619b5ac90c9db0b
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-04-04 00:47:26 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+files=(.MacOSX .gemrc .gitconfig .gitignore_global .gvimrc .pryrc .tmux.conf .vim .vimrc .zsh .zshrc)
+for file in $files;do
+  _path=`basename \`pwd\``/$file
+  ln -s $_path ~/$file
+done
