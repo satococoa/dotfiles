45a89824e20ea19c4df079e0552ae65a5546dbed
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-17 13:39:33 +0900

    add .railsrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-files=(.MacOSX .gemrc .gitconfig .gitignore_global .gvimrc .pryrc .tmux.conf .vim .vimrc .zsh .zshrc)
+files=(.MacOSX .gemrc .gitconfig .gitignore_global .gvimrc .pryrc .tmux.conf .vim .vimrc .zsh .zshrc .railsrc)
 for file in $files;do
   _path=`basename \`pwd\``/$file
   ln -s $_path ~/$file
