2e6963c7560a5321ac8c45672a95243784eb974a
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-09-15 23:18:04 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+echo '(cd ~/dotfiles && git pull)'
+(cd ~/dotfiles && git pull)
+echo 'brew update'
 brew update
+echo 'brew upgrade'
 brew upgrade
+echo 'gem update'
 gem update
+echo 'sudo motion update'
 sudo motion update
