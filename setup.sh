d31de65dec758f5c5e95e01a5e3234dd3296eb12
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-12-27 11:41:41 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
+DIR=$(cd $(dirname $0); pwd)
 files=(.MacOSX .gemrc .gitconfig .gitignore_global .gvimrc .powconfig .pryrc .tmux.conf .vim .vimrc .zsh .zshrc .railsrc update.sh)
-for file in $files;do
-  _path=`basename \`pwd\``/$file
+for file in ${files[@]};do
+  _path=$DIR/$file
   ln -s $_path ~/$file
 done
-cd
-mv ~/.gitignore_global ~/.gitignore
+if ! [ -d ~/.rbenv ]; then
+  mkdir ~/.rbenv
+fi
+ln -s $DIR/.rbenv/default-gems ~/.rbenv/default-gems
+$(cd ~; mv ~/.gitignore_global ~/.gitignore)
