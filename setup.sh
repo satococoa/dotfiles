ac7accaed4c504bd2d266301f3a27bc79be0921a
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-08-09 01:59:10 +0900

    Remove unneeded files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 DIR=$(cd $(dirname $0); pwd)
-files=(.MacOSX .gemrc .gitconfig .gitignore_global .gvimrc .powconfig .pryrc .tmux.conf .vim .vimrc .zsh .zshrc .railsrc update.sh)
+files=(.gemrc .gitconfig .gitignore_global .gvimrc .tmux.conf .vim .vimrc .zsh .zshrc update.sh)
 for file in ${files[@]};do
   _path=$DIR/$file
   ln -s $_path ~/$file
