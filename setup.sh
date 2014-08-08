6221d37b6f225109c232f9d6240da832a12bc78e
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-08-09 02:46:02 +0900

    Introduce homebrew-cask

--- setup.sh
+++ setup.sh
@@ -4,6 +4,33 @@ DIR=$(cd $(dirname $0); pwd)
 files=(.gemrc .gitconfig .gitignore_global .gvimrc .tmux.conf .vim .vimrc .zsh .zshrc update.sh)
 for file in ${files[@]};do
   _path=$DIR/$file
+  echo "ln -s $_path ~/$file"
   ln -s $_path ~/$file
 done
 $(cd ~; mv ~/.gitignore_global ~/.gitignore)
+
+# homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap motemen/ghq
+brew tap peco/peco
+brew tap sanemat/font
+
+brews=(ghq git go hub jq peco rbenv ruby-build reattach-to-user-namespace ricty tmux z zsh-completions)
+
+for brew in ${brews[@]}; do
+  echo "brew install $brew ..."
+  brew install $brew
+done
+
+# cask
+brew install caskroom/cask/brew-cask
+
+casks=(alfred android-file-transfer appcleaner atom butter crashlytics dropbox google-chrome hipchat iterm2 licecap testflight vagrant virtualbox)
+for cask in ${casks[@]}; do
+  echo "brew cask install $cask ..."
+  brew cask install $cask
+done
+brew cask alfred link
--- update.sh
+++ update.sh
@@ -5,7 +5,13 @@ echo 'brew update'
 brew update
 echo 'brew upgrade'
 brew upgrade
+echo 'brew cleanup'
+brew cleanup
+echo 'brew cask cleanup'
+brew cask cleanup
 echo 'gem update'
 gem update
+echo 'gem cleanup'
+gem cleanup
 echo 'sudo motion update'
 sudo motion update
