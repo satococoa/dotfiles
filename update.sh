50f52a650e836c75a70119fe9e416cfe2e8ab9d5
Satoshi Ebisawa <e.satoshi@gmail.com>
2015-05-02 02:52:48 +0900

    Add --all option to brew upgrade

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ echo '(cd ~/dev/src/github.com/satococoa/dotfiles && git pull)'
 (cd ~/dev/src/github.com/satococoa/dotfiles && git pull)
 echo 'brew update'
 brew update
-echo 'brew upgrade'
-brew upgrade
+echo 'brew upgrade --all'
+brew upgrade --all
 echo 'brew cleanup'
 brew cleanup
 echo 'brew cask cleanup'
