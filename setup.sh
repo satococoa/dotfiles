b0d81b040dcd40aab0ff2666db05d2fba8dbbcd1
Satoshi Ebisawa <e.satoshi@gmail.com>
2017-01-02 20:42:58 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ for file in ${files[@]};do
   echo "ln -s $_path ~/$file"
   ln -s $_path ~/$file
 done
+_fish_path=$DIR/fish
+echo "ln -s $_fish_path ~/.config/"
+ln -s $_fish_path ~/.config/
+
 $(cd ~; mv ~/.gitignore_global ~/.gitignore)
 
 # homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-brew tap motemen/ghq
-brew tap peco/peco
-
-brews=(ghq git go hub jq peco readline openssl rbenv ruby-build rbenv-gemset reattach-to-user-namespace tmux z zsh-completions direnv mercurial)
+brews=(ghq git go hub jq peco readline openssl rbenv ruby-build reattach-to-user-namespace tmux direnv mercurial)
 
 for brew in ${brews[@]}; do
   echo "brew install $brew ..."
   brew install $brew
 done
-
