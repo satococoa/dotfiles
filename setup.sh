08ccef8dd042fb2635724035e9908064236516c0
Satoshi Ebisawa <e.satoshi@gmail.com>
2017-09-11 14:07:52 +0900

    Use bash

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/bin/zsh
 
 DIR=$(cd $(dirname $0); pwd)
-files=(.gemrc .gitconfig .gitignore_global .tmux.conf .vim .vimrc .update.sh)
+files=(.gemrc .gitconfig .gitignore_global .tmux.conf .vim .vimrc .update.sh .bash_profile .bashrc)
 for file in ${files[@]};do
   _path=$DIR/$file
   echo "ln -s $_path ~/$file"
   ln -s $_path ~/$file
 done
-_fish_path=$DIR/fish
-echo "ln -s $_fish_path ~/.config/"
-ln -s $_fish_path ~/.config/
 
 $(cd ~; mv ~/.gitignore_global ~/.gitignore)
 
