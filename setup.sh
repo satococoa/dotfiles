9db385559099609d3dccc2707714a1a7566836fa
Satoshi Ebisawa <e.satoshi@gmail.com>
2022-07-22 12:49:24 +0900

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,13 @@ DIR=$(cd $(dirname $0); pwd)
 files=(.tmux.conf update.sh .zshrc)
 for file in ${files[@]};do
   _path=$DIR/$file
-  echo "ln -s $_path ~/$file"
-  ln -s $_path ~/$file
+  _target=~/$file
+  if [[ -f $_target ]]; then
+    echo "remove exsisting file: $_target"
+    rm $_target
+  fi
+  echo "ln -s $_path $_target"
+  ln -s $_path $_target
 done
 
 # .config
@@ -13,8 +18,13 @@ mkdir -p ~/.config
 config_dirs=(git)
 for config_dir in ${config_dirs[@]};do
   _path=$DIR/$config_dir
-  echo "ln -s $_path ~/.config/$config_dir"
-  ln -s $_path ~/.config/$config_dir
+  _target=~/.config/$config_dir
+  if [[ -d $_target ]]; then
+    echo "remove exsisting dir: $_target"
+    rm -rf $_target
+  fi
+  echo "ln -s $_path $_target"
+  ln -s $_path $_target
 done
 
 # homebrew
