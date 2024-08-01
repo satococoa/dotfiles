1c7cc75831a59e1757f2267fa1fdad7cc6e8ba6a
Satoshi Ebisawa <e.satoshi@gmail.com>
2024-08-01 23:19:55 +0900

    setup zed

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ for file in ${files[@]};do
   ln -s $_path $_target
 done
 
-# .config
+# .config/ にディレクトリごとシンボリックリンクを貼る
 mkdir -p ~/.config
 config_dirs=(git alacritty zellij)
 for config_dir in ${config_dirs[@]};do
@@ -27,15 +27,28 @@ for config_dir in ${config_dirs[@]};do
   ln -s $_path $_target
 done
 
-# homebrew
-brews=(ghq git jq peco direnv zsh-completions zellij asdf pure go ripgrep)
+# .config/ 内に特定ファイルのシンボリックリンクを貼る
+mkdir -p ~/.config
+config_files=(zed/settings.json zed/keymap.json)
+for config_file in ${config_files[@]};do
+  _path=$DIR/$config_file
+  _target=~/.config/$config_file
+  if [[ -f $_target ]]; then
+    echo "remove exsisting file: $_target"
+    rm $_target
+  fi
+  echo "ln -s $_path $_target"
+  ln -s $_path $_target
+done
 
+# homebrew
+brews=(ghq git jq peco direnv zsh-completions zellij asdf pure go ripgrep font-udev-gothic)
 for brew in ${brews[@]}; do
   echo "brew install $brew ..."
   brew install $brew
 done
 
-casks=(1password appcleaner dash discord docker figma google-chrome google-drive imageoptim multipass raindropio visual-studio-code)
+casks=(1password appcleaner discord docker figma google-chrome imageoptim raindropio visual-studio-code zed)
 for brew in ${casks[@]}; do
   echo "brew install $brew ..."
   brew install $brew
